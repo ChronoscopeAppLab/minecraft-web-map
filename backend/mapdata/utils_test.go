6bcef9482ca6d05a4d6852183ee7da257e0d21d7
Koki Fukuda <ko.fu.dev@gmail.com>
2021-02-21 20:51:42 +0900

    backend: Add test

---
+++ backend/mapdata/utils_test.go
@@ -0,0 +1,26 @@
+// Copyright (C) 2021 Chronoscope. All rights reserved.
+
+package mapdata
+
+import "testing"
+
+func TestSkipSpace(t *testing.T) {
+	i := skipSpace("  a", 0)
+	if i != 2 {
+		t.Errorf("Expected 2, but got %d\n", i)
+	}
+}
+
+func TestSkipSpaceMixed(t *testing.T) {
+	i := skipSpace(" \t a", 0)
+	if i != 3 {
+		t.Errorf("Expected 3, but got %d\n", i)
+	}
+}
+
+func TestSkipSpaceNotHead(t *testing.T) {
+	i := skipSpace(" a  b", 2)
+	if i != 4 {
+		t.Errorf("Expected 4, but got %d\n", i)
+	}
+}
