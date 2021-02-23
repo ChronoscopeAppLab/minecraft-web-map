90443b4c61c1269a48d92b5d7fdfc1cc9c1519c3
Koki Fukuda <ko.fu.dev@gmail.com>
2021-02-24 01:05:45 +0900

    backend: Handle signals

--- backend/signal_linux.go
+++ backend/signal_linux.go
@@ -8,19 +8,30 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/ChronoscopeAppLab/minecraft-web-map/backend/env"
 	"github.com/ChronoscopeAppLab/minecraft-web-map/backend/mapdata"
 )
 
 func initSignalHandlers() {
 	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGWINCH)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGWINCH, syscall.SIGUSR1)
 
 	go func() {
 		for {
-			<-sigs
-			if err := mapdata.ReloadMetadata(); err != nil {
-				log.Println(err)
-				log.Println("Map metadata wasn't updated")
+			sig := <-sigs
+			switch (sig) {
+			case syscall.SIGWINCH:
+				if err := mapdata.ReloadMetadata(); err != nil {
+					log.Println(err)
+					log.Println("Map metadata wasn't updated")
+				}
+			case syscall.SIGINT:
+				fallthrough
+			case syscall.SIGUSR1:
+				if !env.Debug {
+					os.Remove(env.SocketPath)
+				}
+				os.Exit(0)
 			}
 		}
 	}()
