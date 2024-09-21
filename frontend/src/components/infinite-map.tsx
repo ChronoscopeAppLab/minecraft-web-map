1a9d8694d2aca526ec38359f3ead3c3538c17267
Koki Fukuda <ko.fu.dev@gmail.com>
2024-09-21 23:09:30 +0900

    Move description card to React world

--- frontend/src/components/infinite-map.tsx
+++ frontend/src/components/infinite-map.tsx
@@ -14,6 +14,8 @@ const InfiniteMap = ({prefix}: Props) => {
   const [spots, setSpots] = useState<Spot[]>([]);
   const [scale, setScale] = useState(1);
   const [pos, setPos] = useState({x: 0, z: 0});
+  const [description, setDescription] = useState<Pick<Spot, 'name' | 'x' | 'z' | 'detail'>>({x: 0, z: 0, name: '', detail: ''});
+  const [showDescription, setShowDescription] = useState(false);
 
   useEffect(() => {
     if (!canvasRef.current) {
@@ -28,6 +30,14 @@ const InfiniteMap = ({prefix}: Props) => {
 
       map.setOnScaleChangeCallback(setScale);
       map.setOnCursorMoveCallback(setPos);
+      map.setDescriptionCallback((spot: Spot | null) => {
+        if (spot === null) {
+          setShowDescription(false);
+        } else {
+          setDescription(spot);
+          setShowDescription(true);
+        }
+      });
 
       map.bind(canvasRef.current, prefix, spots);
       setMap(map);
@@ -50,6 +60,16 @@ const InfiniteMap = ({prefix}: Props) => {
         <img src="/images/zoom_in.png" id="zoom-in-button" alt="+" onClick={() => map.zoomIn()} />
         <img src="/images/trip_origin.png" id="zoom-orig-button" alt="0" onClick={() => map.zoomOrig()} />
         <img src="/images/zoom_out.png" id="zoom-out-button" alt="-" onClick={() => map.zoomOut()} />
+      </div>
+
+      <div className={`description-card ${showDescription ? '' : 'hidden'}`} id="description-card">
+        <div id="description-place-name">{description.name}</div>
+        <div id="description-detail">
+          <div id="description-coordinate">
+            X={description.x} Z={description.z}
+          </div>
+          <div id="description-address">{description.detail}</div>
+        </div>
       </div>
 
       <div id="map-status">
